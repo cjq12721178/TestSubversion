63fca44befb924614e5b7b2ed8e4136801fbe531
cjq12721178 <cjq08122108@sina.cn>
2016-07-05 08:33:09 +0800

    modify second activity
    add a log

--- app/src/main/java/com/example/kat/testsubversion/NewActivity.java
+++ app/src/main/java/com/example/kat/testsubversion/NewActivity.java
@@ -9,5 +9,7 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        
+        Log.d("TestSubversion", "modified on internet");
     }
 }
